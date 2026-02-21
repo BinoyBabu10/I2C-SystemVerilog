fab192da714d6d59d66885b38a16073a258d5d18
Binoy Babu K <140301330+BinoyBabu10@users.noreply.github.com>
2026-02-21 21:09:51 +0530

    Update design.sv

--- design.sv
+++ design.sv
@@ -340,3 +340,14 @@ endmodule
                           
                 
                         
+interface i2c_if;
+  logic clk;
+  logic rst;
+  logic newd;
+  logic op;
+  logic [7:0]din;
+  logic [6:0]addr;
+  logic [7:0]dout;
+  logic done;
+  logic busy,ack_err;
+endinterface
